d1f1f62e7f6cdb0b1bf9c2fdf37e8cd2e2db737c
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-02-22 23:13:02 +0100

    Use Redis instead

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
+const redis = require('redis');
+
+const client = redis.createClient();
 
 const port = process.env.PORT || 3030;
 
@@ -14,17 +17,21 @@ if (process.env.NODE_ENV === 'production') {
 
 // END SETUP
 
-let wallMessage = 'The Wall'
-
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 server.listen(port);
 
+client.setnx('wall-message', 'The Wall with Redis');
+
 io.on('connection', function (socket) {
-  socket.emit('server-message', { message: wallMessage });
+  client.get('wall-message', (err, reply) => {
+    wallMessage = reply;
+    socket.emit('server-message', { message: wallMessage });
+  });
+
   socket.on('client-message', function (data) {
-    wallMessage = data.message;
-    socket.broadcast.emit('server-message', { message: wallMessage });
+    client.set('wall-message', data.message);
+    socket.broadcast.emit('server-message', { message: data.message });
   });
 });
